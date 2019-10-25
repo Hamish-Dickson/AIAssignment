2599d1af378c0914ce349b2704b97dcf5830b0c7
Ryan Junor <ryanjjunor@gmail.com>
2019-10-25 23:50:37 +0100

    Solutions now hold a list of the colours and a list of the permutation.

--- drawColours.py
+++ drawColours.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import os
+import copy
 
 
 # Reads the file  of colours
@@ -48,49 +49,57 @@ def calculate_distance(colour1, colour2):
 def evaluate(solution):
     total_distance = 0
 
-    for i in range(0, len(solution) - 1):
-        total_distance += calculate_distance(solution[i], solution[i + 1])
+    for i in range(0, len(solution[0]) - 1):
+        total_distance += calculate_distance(solution[0][i], solution[0][i + 1])
 
     return total_distance
 
 
 def local_optima(solution):
-    original = solution.copy()
+    original = copy.deepcopy(solution)
 
     original_distance = evaluate(original)
 
-    neighbour = solution.copy()
+    neighbour = copy.deepcopy(solution)
 
     is_optima = True
 
-    for i in range(0, len(solution) - 1):
+    for i in range(0, (len(solution[0]) - 1)):
+
         # assign temp vars to hold array contents to perform switch
-        temp_first = neighbour[i].copy()
-        temp_second = neighbour[i + 1].copy()
+        temp_first = copy.deepcopy(neighbour[0][i])
+        temp_second = copy.deepcopy(neighbour[0][i + 1])
 
         # switch values in array
-        neighbour[i] = temp_second
-        neighbour[i + 1] = temp_first
+        neighbour[0][i] = temp_second
+        neighbour[0][i + 1] = temp_first
 
         # if the created neighbour has less distance than the original solution, the local optima has not been found
         if evaluate(neighbour) < original_distance:
             is_optima = False
             break
 
-        neighbour = original.copy()
+        neighbour = copy.deepcopy(original)
 
     return is_optima
 
 
 def random_neighbour(solution):
-    neighbour = solution.copy()
-    position_to_flip = random.randint(0, len(solution) - 2)
+    neighbour = copy.deepcopy(solution)
+    position_to_flip = random.randint(0, len(solution[0]) - 2)
 
-    temp_first = neighbour[position_to_flip].copy()
-    temp_second = neighbour[position_to_flip + 1].copy()
+#IS COPY NEEDED FOR THE NEXT 4 LINES?*******************************************************************************
+    temp_first_col = copy.deepcopy(neighbour[0][position_to_flip])
+    temp_second_col = copy.deepcopy(neighbour[0][position_to_flip + 1])
 
-    neighbour[position_to_flip] = temp_second
-    neighbour[position_to_flip + 1] = temp_first
+    temp_first_perm = copy.deepcopy(neighbour[1][position_to_flip])
+    temp_second_perm = copy.deepcopy(neighbour[1][position_to_flip + 1])
+
+    neighbour[0][position_to_flip] = temp_second_col
+    neighbour[0][position_to_flip + 1] = temp_first_col
+
+    neighbour[1][position_to_flip] = temp_second_perm
+    neighbour[1][position_to_flip + 1] = temp_first_perm
 
     return neighbour
 
@@ -99,45 +108,58 @@ def random_neighbour(solution):
 
 # Get the directory where the file is located
 dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)  # Change the working directory so we can read the file
+os.chdir(dir_path)  # Change the working directory so we can read the file4---------------------------------
 
 ncolors, colours = read_file('colours.txt')  # Total number of colours and list of colours
 
-test_size = 5  # Size of the subset of colours for testing
+test_size = 20  # Size of the subset of colours for testing
 test_colours = colours[0:test_size]  # list of colours for testing
 
-# permutation is simply order of elements to be chosen I.E 0, 1, 4, 5. could change to 0, 1, 2, 3 for testing
-permutation = random.sample(range(test_size),
-                            test_size)  # produces random pemutation of lenght test_size, from the numbers 0 to test_size -1
-#plot_colours(test_colours, permutation)
+
+# plot_colours(test_colours, permutation)
 
 
 def random_hill_climbing():
+    random_cols = []
 
-    random_sol = []
+    # permutation is simply order of elements to be chosen I.E 0, 1, 4, 5. could change to 0, 1, 2, 3 for testing
+    permutation = random.sample(range(test_size),
+                                test_size)  # produces random pemutation of lenght test_size, from the numbers 0 to test_size -1
 
     for i in range(len(test_colours)):
-        random_sol.append(test_colours[permutation[i]])
+        random_cols.append(test_colours[permutation[i]])
+
+    random_sol = [random_cols, permutation]
+    print("Initial Permutation:", random_sol[1])
+
+    print("Initial:", random_sol)
 
     best_distance = evaluate(random_sol)
-    print(best_distance)
+    print("Initial Distance:", best_distance)
 
-    sol = random_sol.copy()
+    sol = copy.deepcopy(random_sol)
 
     while not local_optima(sol):
 
+        #This was changing sol
         neighbour = random_neighbour(sol)
 
         neighbour_distance = evaluate(neighbour)
 
         if neighbour_distance < best_distance:
-
             sol = neighbour
+
             best_distance = neighbour_distance
+
+            print("New Permutation:", sol[1])
+            print("New Distance:", evaluate(sol))
+            print("New Sol:", sol)
 
     return sol
 
 
+solution2 = random_hill_climbing()
 
-random_hill_climbing()
-
+print("Final Permutation:", solution2[1])
+print("Final:", solution2)
+print("Final Distance:", evaluate(solution2))
